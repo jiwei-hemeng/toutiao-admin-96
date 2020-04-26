44bebd64a92d51e1f6b0c75f146b750e1a8a3cba
阆苑 <3174541442@qq.com>
2020-04-26 19:05:26 +0800

    图片素材

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,15 +1,135 @@
 <template>
-  <div>素材管理</div>
+  <div class="image-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包屑路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>素材管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- /面包屑路径导航 -->
+        <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+      </div>
+      <div class="action-head">
+        <el-radio-group
+          v-model="collect"
+          size="mini"
+          @change="onCollectChange"
+        >
+          <el-radio-button
+            :label="false"
+          >全部</el-radio-button>
+          <el-radio-button
+            :label="true"
+          >收藏</el-radio-button>
+        </el-radio-group>
+        <el-button
+          size="mini"
+          type="success"
+          @click="dialogUploadVisible = true"
+        >上传素材</el-button>
+      </div>
+      <!-- 素材列表 -->
+      <el-row :gutter="10">
+        <el-col
+          :xs="12"
+          :sm="6"
+          :md="6"
+          :lg="4"
+          v-for="(img, index) in images"
+          :key="index"
+        >
+          <el-image
+            style="height: 100px"
+            :src="img.url"
+            fit="cover"
+          ></el-image>
+        </el-col>
+      </el-row>
+      <!-- /素材列表 -->
+    </el-card>
+
+    <el-dialog
+      title="上传素材"
+      :visible.sync="dialogUploadVisible"
+      :append-to-body="true"
+    >
+      <!--
+        upload 组件本身就支持请求提交上传操作，说白了你使用它不用自己去发请求，它会自己发。
+        请求方法：默认就是 POST
+        请求路径：action，必须写完整路径
+        请求头：headers
+       -->
+      <el-upload
+        class="upload-demo"
+        drag
+        action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+        :headers="uploadHeaders"
+        name="image"
+        multiple
+        :show-file-list="false"
+        :on-success="onUploadSuccess"
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { getImages } from '@/api/image'
+
 export default {
-  name: 'imageIndex',
-  data () {},
-  methods: {}
+  name: 'ImageIndex',
+  components: {},
+  props: {},
+  data () {
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    return {
+      collect: false, // 默认查询全部素材列表
+      images: [], // 图片素材列表
+      dialogUploadVisible: false,
+      uploadHeaders: {
+        Authorization: `Bearer ${user.token}`
+      }
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    this.loadImages(false)
+  },
+  mounted () {},
+  methods: {
+    loadImages (collect = false) {
+      getImages({
+        collect
+      }).then(res => {
+        this.images = res.data.data.results
+      })
+    },
+
+    onCollectChange (value) {
+      this.loadImages(value)
+    },
+
+    onUploadSuccess () {
+      // 关闭对话框
+      this.dialogUploadVisible = false
+
+      // 更新素材列表
+      this.loadImages(false)
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
-
+.action-head {
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
